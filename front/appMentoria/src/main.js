109dce6a75cd90e992677f5a367fa0856938a90e
Climent Fernández <a23cliferand@inspedralbes.cat>
2025-02-06 22:27:12 +0100

    Add i18n support: integrate vue-i18n plugin, update language files for special character handling, and enhance main.js for localization

--- front/appMentoria/src/main.js
+++ front/appMentoria/src/main.js
@@ -1,5 +1,13 @@
+// import './assets/m</div>ain.css'
+// import './assets/main.css'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
+import router from './router'
+import './assets/tailwind.css'
+import "../node_modules/flag-icons/css/flag-icons.min.css";
+
 import { createI18n } from 'vue-i18n'
 import es from './assets/languages/es.json'
 import ca from './assets/languages/ca.json'
@@ -10,25 +18,38 @@ import pt from './assets/languages/pt.json'
 import ja from './assets/languages/ja.json'
 import eu from './assets/languages/eu.json'
 import gl from './assets/languages/gl.json'
+import en from './assets/languages/en.json'
 
-const messages = {
-  es,
-  ca,
-  zh,
-  de,
-  ur,
-  pt,
-  ja,
-  eu,
-  gl
-}
 
-const i18n = createI18n({
-  locale: 'ca', // idioma por defecto
-  fallbackLocale: 'es',
-  messages,
-})
+
+
 
 const app = createApp(App)
+
+const i18n = createI18n({
+    locale: 'ca',
+    messages: {
+        "en":en,
+        "es": es,
+        "ca": ca,
+        "zh": zh,
+        "de": de,
+        "ur": ur,
+        "pt": pt,
+        "ja": ja,
+        "eu": eu,
+        "gl": gl 
+        
+    },
+    
+})
+
 app.use(i18n)
+app.use(createPinia())
+app.use(router)
+
 app.mount('#app')
+
+export function changeLocale(locale) {
+    i18n.global.locale = locale
+}
--- front/appMentoria/vite.config.js
+++ front/appMentoria/vite.config.js
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    vueI18n({
+      include: path.resolve(__dirname, './src/assets/languages/**'),
+      runtimeOnly: false, 
+      compositionOnly: false,
+      fullInstall: true,
+      escapeHtml: true
+    }),
   ],
   resolve: {
     alias: {
