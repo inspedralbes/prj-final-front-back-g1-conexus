3b62341095c706be839e0b500ba09ceda119fae1
SaraMartinez21 <123445716+SaraMartinez21@users.noreply.github.com>
2025-02-06 16:10:47 +0100

    Refactor NavBarWeb component layout for improved responsiveness and visibility

--- front/appMentoria/src/views/mainPage.vue
+++ front/appMentoria/src/views/mainPage.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <Header class="fixed top-0 left-0 right-0 z-10"></Header>
-    <NavBarWeb class="hidden lg:fixed md:w-60 lg:block lg:top-0 py-9 lg:left-0 lg:bottom-auto lg:w-45 lg:h-full lg:rounded-none"></NavBarWeb>
+    <NavBarWeb class="hidden lg:fixed lg:top-0 lg:left-0 lg:h-screen lg:w-60 lg:block"></NavBarWeb>
     <NavBarApp 
       class="fixed bottom-0 left-0 right-0 lg:hidden">
     </NavBarApp>
-    <main class="flex-grow mt-16 mb-20 lg:mb-0 md:ml-20"> 
+    <main class="flex-grow mt-16 mb-20 lg:ml-60 lg:mb-0"> 
       <ViewPostList></ViewPostList>
     </main>
   </div>
 </template>
+
 
 <script setup>
 import NavBarApp from "@/components/navBar/NavBarApp.vue";
