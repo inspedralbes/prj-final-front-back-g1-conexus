72f66102e11f756f516ea91bf37a610acf36aad8
julsluks <a22betvilver@inspedralbes.cat>
2025-02-15 22:13:40 +0100

    Add "Add Post" translation to multiple language files and update NavBarWeb.vue to include the new button

--- front/appMentoria/src/components/navBar/NavBarWeb.vue
+++ front/appMentoria/src/components/navBar/NavBarWeb.vue
@@ -136,6 +136,7 @@
                         d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z"
                         clip-rule="evenodd" />
                     </svg>
+                    {{ $t("navBarWeb.addPost") }}
                   </button>
                 </div>
               <div class="absolute bottom-0 left-0 w-full">
@@ -156,17 +157,16 @@
 import { ref, onMounted, reactive, watch } from 'vue';
 import SubMenu from '../SubMenu.vue';
 import { useAppStore } from '@/stores/index';
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const appStore = useAppStore();
 const route = useRoute();
+const router = useRouter();
 const selectedItem = ref(null);
-
 
 var user = reactive({});
 var profile = ref(null);
 var name = ref(null);
-
 
 onMounted(() => {
   user.value = appStore.getUser();
@@ -196,9 +196,9 @@ watch(route, (newRoute) => {
 }, { immediate: true });
 
 const handleClick = () => {
-  if (router.currentRoute.value.path === '/') {
+  if (route.path === '/') {
     router.push('/addComunityPost');
-  } else if (router.currentRoute.value.path === '/requests') {
+  } else if (route.path === '/requests') {
     router.push('/addRequestPost');
   }
 }
