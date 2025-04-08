3119f834f0b2f39faf6a04f653c897a4411fb3c9
julsluks <julievillegas77@gmail.com>
2025-04-08 12:38:51 +0200

    Remove unused banner and profile images from the upload directory to clean up the repository.

--- back/microprocessos_api/.script.dev.sh
+++
@@ -1,3 +0,0 @@
-echo "Installing root services with npm..."
-ls -d /usr/src/node/services/*/ | xargs -I {} sh -c 'npm install --prefix {} && cp {}/.env.dev {}/.env && echo "Installed {}"'
-echo "Installation complete."
--- back/microprocessos_api/.script.prod.sh
+++
@@ -1,3 +0,0 @@
-echo "Installing root services with npm..."
-ls -d /usr/src/node/services/*/ | xargs -I {} sh -c 'pnpm install {} && cp {}/.env.prod {}/.env && echo "Installed {}"'
-echo "Installation complete."
