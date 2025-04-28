da7b74292bd4d5326548310e9bc03c0d5f045298
julsluks <julievillegas77@gmail.com>
2025-04-28 12:23:47 +0200

    fix: correct table name and foreign key reference in insert_db.sql; clean up whitespace in index.js

--- back-node/node/index.js
+++ back-node/node/index.js
@@ -23,7 +23,6 @@ import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 dotenv.config();
 
 const app = express();
@@ -35,7 +34,6 @@ app.use(cors());
 app.use("/api/assistences", assistenceRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/departments", departmentRoutes);
-
 
 app.use("/api/lost-objects", lostObjectRoutes);
 app.use("/api/reports", reportRoutes);
