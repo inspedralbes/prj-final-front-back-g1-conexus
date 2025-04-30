f84cf282636be27f6bef1a5e20bffff014f7d629
rublormar <a21rublormar@inspedralbes.cat>
2025-04-30 12:40:04 +0200

    fix: update database configuration to use correct environment variable
    - Changed NODE_DB_PASSWORD to NODE_DB_ROOT_PASSWORD in the Sequelize initialization to align with updated environment variable naming conventions.

--- back-node/node/config/database.js
+++ back-node/node/config/database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.NODE_DB_NAME, process.env.NODE_DB_USER, process.env.NODE_DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.NODE_DB_NAME, process.env.NODE_DB_USER, process.env.NODE_DB_ROOT_PASSWORD, {
     host: process.env.NODE_DB_HOST,
     dialect: "mysql",
 });
