6e4e01239e51519e1fc8d2c53e7d60b144d2238c
julsluks <julievillegas77@gmail.com>
2025-05-06 08:51:06 +0200

    refactor: update bcrypt dependency from bcryptjs to bcrypt

--- back-node/node/routes/userRoutes.js
+++ back-node/node/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import multer from "multer";
 import path from "path";
 import fs from "fs";
