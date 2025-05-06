8e0a23e8d2da57b7807271c0a020bb4bd622a64e
julsluks <julievillegas77@gmail.com>
2025-05-06 08:33:42 +0200

    refactor: remove unused import for HomeView in router configuration

--- front-vue/src/router/index.js
+++ front-vue/src/router/index.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../views/Teachers/myCourses.vue'
-
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
