f55d9100784b446c6839c8e8811aee0217534397
julsluks <julievillegas77@gmail.com>
2025-05-13 11:20:48 +0200

    feat: Add course and lost object routes to the server setup

--- back-node/node/lostObjects.js
+++ back-node/node/lostObjects.js
@@ -2,15 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import { sequelize } from "./models/index.js";
 import dotenv from 'dotenv';
-import path from 'path';
 import bodyParser from 'body-parser';
+import lostObjectRoutes from './routes/lostObjectRoutes.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.NODE_LOST_OBJECT_PORT || 3005;
+
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/api/lost-objects', lostObjectRoutes);
+
 sequelize.sync().then(() => {
     console.log("Database synced");
     app.listen(PORT, () => {
