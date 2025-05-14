b37fca34faf9fa44c5f6d7e754caa3b85f790c99
Eric Gómez Vilà <105845066+EGVPedralbes@users.noreply.github.com>
2025-05-14 12:22:53 +0200

    Add get enabled

--- front-vue/src/services/communicationsScripts/canteenComManager.js
+++ front-vue/src/services/communicationsScripts/canteenComManager.js
@@ -89,3 +89,21 @@ export const deleteItem=async (id) => {
         console.error('Error deleting course:', error);
     }
 }
+export const getAllEnabledCanteenItems=async () => {
+    try {
+        const response = await fetch(`${BACK_URL}api/canteen/enabled`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching courses:', error);
+    }
+
+}
